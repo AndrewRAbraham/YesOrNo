933036fce5bdcb8e47d2e7b586ee8faba6db1998
AndrewRAbraham <andrew.abraham.r@gmail.com>
2016-04-09 18:14:54 -0400

    Round 4
    Go

--- onSubmitButton.js
+++ onSubmitButton.js
@@ -1,1 +1,25 @@
-console.log("success!"); 
+//var currentLocation = window.location;
+
+console.log(typeof $);
+
+
+
+
+
+
+
+
+
+
+/*currentLocation.
+var actualCode = '(' + function() {
+    // All code is executed in a local scope.
+    // For example, the following does NOT overwrite the global `alert` method
+    var alert = null;
+    // To overwrite a global variable, prefix `window`:
+    window.alert = null;
+} + ')();';
+var script = document.createElement('script');
+script.textContent = actualCode;
+(document.head||document.documentElement).appendChild(script);
+script.parentNode.removeChild(script);
